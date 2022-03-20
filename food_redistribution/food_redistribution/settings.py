e9f1616e00f028da246a91e740d3ec392b1cb41d
Satyen Singh <srs833@nyu.edu>
2022-03-19 23:01:19 -0400

    email verification working again

--- food_redistribution/food_redistribution/settings.py
+++ food_redistribution/food_redistribution/settings.py
@@ -141,10 +141,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 load_dotenv()
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "foodredis.5@gmail.com"
-EMAIL_HOST_PASSWORD = str(os.getenv("EMAIL_CREDS"))
+EMAIL_HOST_USER = "smash5foodred@gmail.com"
+EMAIL_HOST_PASSWORD = str(os.getenv("PASSWORD"))
 EMAIL_PORT = 587
 
 django_heroku.settings(locals(), test_runner=False)
