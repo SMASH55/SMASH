2e37de07896a5b929be152cc4ef7db5fcfefe3a4
Satyen Singh <srs833@nyu.edu>
2022-06-20 21:37:01 -0400

    fixed registration - AGAIN

--- food_redistribution/food_redistribution/settings.py
+++ food_redistribution/food_redistribution/settings.py
@@ -145,7 +145,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = "smash5foodred@gmail.com"
-EMAIL_HOST_PASSWORD = str(os.getenv("PASSWORD"))
+EMAIL_HOST_PASSWORD = str(os.getenv("APP_PASSWORD"))
+# EMAIL_APP_PASSWORD = str(os.getenv("APP_PASSWORD"))
 EMAIL_PORT = 587
 
 django_heroku.settings(locals(), test_runner=False)
